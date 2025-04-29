e2411bfc037c7ccbd8e1ee8d6201a149d0deac1f
jung33010 <jung3301@gmail.com>
2025-04-29 10:09:05 +0900

    Update.

--- JeonJungKyu/lastlastlast.py
+++ JeonJungKyu/lastlastlast.py
@@ -15,7 +15,7 @@ NAVER_CLIENT_ID = os.getenv("NAVER_CLIENT_ID")
 NAVER_CLIENT_SECRET = os.getenv("NAVER_CLIENT_SECRET")
 OPENAI_API_KEY = os.getenv("OPEN_AI_API")
 
-client = OpenAI(api_key=OPENAI_API_KEY)  # ⭐ 신버전 스타일로 클라이언트 생성
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 db_config = {
     'host': os.getenv('DB_HOST'),
